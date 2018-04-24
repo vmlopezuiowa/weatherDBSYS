1aaa56a99b8b5e18f2e04add5fec6e0c82a273ff
nicholaslgoodman <i@nicholasleegoodman.com>
2018-04-24 16:10:29 -0500

    weather rise entity registration

--- WeatherApp/Data/ApplicationDbContext.cs
+++ WeatherApp/Data/ApplicationDbContext.cs
@@ -15,6 +15,8 @@ namespace WeatherApp.Data
         {
         }
         public DbSet<Weather> Weather { get; set; }
+        public DbSet<Weather> WeatherRise { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
